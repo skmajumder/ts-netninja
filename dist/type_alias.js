b2ef3b2c095c7058f38475144dca4aa3776f0852
skmajumder <skmajumder.cse@gmail.com>
2024-04-21 20:33:52 +0600

    Type Aliases

---
+++ dist/type_alias.js
@@ -0,0 +1,15 @@
+"use strict";
+// * -----------------
+// * Type Aliases
+// * -----------------
+function generateRandomColor() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    return [red, green, blue, 1];
+}
+console.log(generateRandomColor());
+// * -----------------
+// * Type Aliases - Object literal
+// * -----------------
+//# sourceMappingURL=type_alias.js.map
---
+++ src/type_alias.ts
@@ -0,0 +1,50 @@
+// * -----------------
+// * Type Aliases
+// * -----------------
+
+// type RGBA = {
+//   red: number;
+//   green: number;
+//   blue: number;
+//   alpha: number;
+// };
+
+// function generateRandomColor(): RGBA {
+//   const red = Math.floor(Math.random() * 255);
+//   const green = Math.floor(Math.random() * 255);
+//   const blue = Math.floor(Math.random() * 255);
+
+//   return { red, green, blue, alpha: 1 };
+// }
+
+// console.log(generateRandomColor());
+
+// const colorBlue: RGBA = {
+//   red: 0,
+//   green: 0,
+//   blue: 255,
+//   alpha: 1,
+// };
+
+// const colorMagenta: RGBA = {
+//   red: 255,
+//   green: 0,
+//   blue: 255,
+//   alpha: 1,
+// };
+
+type RGBA = [number, number, number, number];
+
+function generateRandomColor(): RGBA {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return [red, green, blue, 1];
+}
+
+console.log(generateRandomColor());
+
+// * -----------------
+// * Type Aliases - Object literal
+// * -----------------
