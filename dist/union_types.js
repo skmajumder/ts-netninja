8aea8d26dc11d43ad93ee9d3501e247e220e6eaf
skmajumder <skmajumder.cse@gmail.com>
2024-04-23 01:37:17 +0600

    Union types

---
+++ dist/union_types.js
@@ -0,0 +1,11 @@
+"use strict";
+// * -----------------
+// * Union Types
+// * -----------------
+let userId;
+userId = "HG240788";
+userId = 8;
+let userEmail;
+userEmail = "user@example.com";
+userEmail = null;
+//# sourceMappingURL=union_types.js.map
---
+++ src/union_types.ts
@@ -0,0 +1,11 @@
+// * -----------------
+// * Union Types
+// * -----------------
+
+let userId: string | number;
+userId = "HG240788";
+userId = 8;
+
+let userEmail: string | null;
+userEmail = "user@example.com";
+userEmail = null;
