dbb9e02222139912d210b5d0c98d3f8ad73eda5f
skmajumder <skmajumder.cse@gmail.com>
2024-09-22 21:31:11 +0600

    setup

--- dist/array.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// * TypeScript Arrays
-let specificTypeArr = [
-    { property1: 10, property2: "Ten" },
-    { property1: 20, property2: "Twenty" },
-];
-//# sourceMappingURL=array.js.map
