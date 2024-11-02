f57805abd44bd211f90db164a98e86265e623e79
skmajumder <skmajumder.cse@gmail.com>
2024-11-02 18:44:39 +0600

    Object

---
+++ dist/objects.js
@@ -0,0 +1,33 @@
+"use strict";
+// * Arrays
+// let names: string[] = ["Bangladesh", "India", "Sri Lanka", "Nepal"];
+// names.push("Bhutan");
+// console.log(names);
+// names.push("Bhutan");
+// console.log(names);
+// names.pop();
+// console.log(names);
+// names.shift();
+// console.log(names);
+const everything = [10, "10, true", "10, false", "20", true, null, undefined];
+everything.push("100");
+// console.log(everything);
+const t = everything[1];
+console.log(t);
+// let value: unknown;
+// value = 20;
+// value = "HELLO, WORLD!";
+// console.log(typeof value);
+// if (Array.isArray(value)) {
+//   value.push("New value");
+//   console.log(value);
+// }
+// if (typeof value === "string") {
+//   const newValue = value.toLowerCase();
+//   console.log(newValue);
+// }
+// console.log(value);
+// const userName = "John Doe";
+// const newName = userName.toUpperCase();
+// console.log(userName);
+// console.log(newName);
